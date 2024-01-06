5051487b6f001982fca83ff1d4f0a19946f0518d
Prem <premanand484@gmail.com>
2024-01-06 16:18:41 +0530

    Merge remote changes

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           {showWelcome &&
            
            <>
-           <Animation/>
+           <Animation/> 
            </>
            
           }         
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,11 +10,11 @@ function Home() {
         <div className="col-lg-5 col-sm-12 mt-5  d-flex justify-content-center ">
           <div className='inner_circle mt-5 '>
             <div className="men-image">
-            <img src='https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw' alt="" className='men-images' />
+              <img src='https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw' alt="" className='men-images' />
             </div>
-        
+
           </div>
-         
+
         </div>
         <div className='col-lg-7 col-sm-12 my-5 py-3'>
           <h1 className='text-light  mt-5 colors'>Hello,
@@ -30,12 +30,12 @@ function Home() {
           </h1>
           <p className='text-light lead text-head '>I am a dedicated Frontend Developer with a passion for creating aesthetically pleasing, user-friendly websites and applications. My journey in the field of web development began with mastering the fundamentals - HTML, CSS, and JavaScript. These foundational languages allowed me to understand the building blocks of the web and how to manipulate them to create engaging user experiences.</p>
 
-        <button className="button mt-4"><span className="button-content">
-          <Link to={"/Prem_Anand.pdf"} 
-          target='_blank'
-          className=' nav-link '
-          >Download CV</Link>
-        </span></button>
+          <a href="/Prem_Anand.pdf" target="_blank" rel="noopener noreferrer" download>
+            <button className="button mt-4">
+              <span className="button-content">Download CV</span>
+            </button>
+          </a>
+
         </div>
       </div>
 
